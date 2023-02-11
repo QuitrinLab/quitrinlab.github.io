792fe445b3dbc80262aac3f4764d000cb49465a9
Quitrindev <87720636+QuitrinDEV@users.noreply.github.com>
2023-02-11 11:48:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const CONTENT = 
+    "<br>"+
     "<h1>Stay tuned</h1>" +
     "<p>Our site is making now!</p>" +
     "<p></p>" +
     "<br>" +
     "<h4>Happy life!</h4>" +
-    "<p>love u.</p>";
+    "<p>love u.</p>" +
+    "<br>";
 
 const CONTENT_BOX = document.getElementById("content");
 
