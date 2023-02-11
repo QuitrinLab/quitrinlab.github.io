16a2a266c3e3de1df97b79a309ac882ffe62d0d2
Quitrindev <87720636+QuitrinDEV@users.noreply.github.com>
2023-02-11 11:10:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const CONTENT = 
+    "<h1>Stay tuned</h1>" +
+    "<p>Our site is making now!</p>" +
+    "<p></p>" +
+    "<br>" +
+    "<h4>Happy life!</h4>" +
+    "<p>love u.</p>";
+
+const CONTENT_BOX = document.getElementById("content");
+
+CONTENT_BOX.innerHTML = "<!--\n\tContent\n\tbegin\n-->\n\n" + CONTENT + "\n\n<!--\n\tend.\n-->";
+
+/*
+<h1>Stay tuned</h1>
+			<p>Our site is making now!</p>
+			<p></p>
+			<br>
+			<h4>Happy life!</h4>
+			<p>love u.</p>
+*/
